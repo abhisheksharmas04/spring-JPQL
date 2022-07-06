6ccfe8576f6293d3807f3d12a93c96c91ba61a67
DeLL <DeLL@Abhishek>
2022-07-06 07:00:02 +0530

    Project Created

---
+++ src/main/java/com/example/demo/BootDataJpaProj6FinderMethodWithProjection1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDataJpaProj6FinderMethodWithProjection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDataJpaProj6FinderMethodWithProjection1Application.class, args);
+	}
+
+}
